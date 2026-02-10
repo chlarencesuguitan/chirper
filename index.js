7f23f1efc9bb2aa77e8ac10b45849e25fb19346a
khukuu <chlarence.suguitan83@gmail.com>
2026-02-10 11:34:36 +0800

    Updated the ID and added Reading
    Updated the Id so it have a relatively long unique ID, updated the Create up to Read

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {v4:uuid} = require('uuid');
 const chirps = [];
-let chirpID = 1;
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
@@ -10,23 +11,30 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
-// app.get('/', (req, res)=>{
-//     res.render('home.ejs')
-// })
-
-app.post('/posts', (req, res)=>{
-  const chirp = {id: chirpID++, content:req.body.content}
-  chirps.unshift(chirp)
+app.post('/posts', (req, res)=> {
+  if(!req.body.content?.trim()){
+    return res.redirect('/');
+  }
+  const chirp = {id: uuid(), content:req.body.content};
+  chirps.unshift(chirp);//push is like parang comment 
   res.redirect('/')
 })
+
+app.get('/posts/:id', (req, res)=>{
+  const { id } = req.params
+  const chirp = chirps.find(c => c.id === id);
+  if (!chirp) return res.redirect('/');
+  res.render('view', { chirp })
+})
+
+
+
+
+
 
 app.get('/', (req, res) =>{
   res.render('home', { chirps })
 })
-
-
-
-
 
 
 app.listen(8080, ()=>{
